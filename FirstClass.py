5a5cf737c744002efbe3ad6aa5dc241257b989c7
3194libin <15764747737@qq.com>
2019-06-21 12:44:40 +0800

    Initial commit

---
+++ FirstClass.py
@@ -0,0 +1,16 @@
+class student:
+    id = '20171103194'
+    sex = 'man'
+    age = 21
+
+    def run(self):
+        print("我跑的很快！")
+
+    def climb(self):
+        print("我很小的时候就会爬！")
+
+    def sleep(self):
+        print("我特别爱睡觉")
+
+s = student()
+s.climb()
